589c1fc1dbee6447856f948db3d79048aa7ca8b5
nirvana-1993 <songojame@gmail.com>
2016-11-21 01:44:38 +0700

    can play youtube video

--- app/src/main/java/app/awitcha/youtube/MainActivity.java
+++ app/src/main/java/app/awitcha/youtube/MainActivity.java
@@ -3,11 +3,102 @@ package app.awitcha.youtube;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    public static final String API_KEY = "AIzaSyAK2ighaln-uQVIMFXDjg6bR8af5CKGiKA";
+
+    private String videoCode = "JWlC8nCYs9k";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        youTubeView.initialize(API_KEY, this);
     }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        player.setPlayerStateChangeListener(playerStateChangeListener());
+        player.setPlaybackEventListener(playbackEventListener());
+        if (!wasRestored) {
+            player.cueVideo(videoCode);
+        }
+    }
+
+    private YouTubePlayer.PlaybackEventListener playbackEventListener() {
+        return new YouTubePlayer.PlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+
+            }
+
+            @Override
+            public void onPaused() {
+
+            }
+
+            @Override
+            public void onStopped() {
+
+            }
+
+            @Override
+            public void onBuffering(boolean b) {
+
+            }
+
+            @Override
+            public void onSeekTo(int i) {
+
+            }
+        };
+    }
+
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener() {
+        return new YouTubePlayer.PlayerStateChangeListener() {
+            @Override
+            public void onLoading() {
+
+            }
+
+            @Override
+            public void onLoaded(String s) {
+
+            }
+
+            @Override
+            public void onAdStarted() {
+
+            }
+
+            @Override
+            public void onVideoStarted() {
+
+            }
+
+            @Override
+            public void onVideoEnded() {
+
+            }
+
+            @Override
+            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+            }
+        };
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
 }
